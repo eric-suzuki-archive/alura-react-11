c22fea4c413e1bf528a09eaa7af3ce89aea5f15f
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-04-03 23:56:05 -0300

    Module 01-Aula 06

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+import { Estilos } from './componentes/EstilosGlobais/Estilos';
 
 function App() {
     return (
         <div>
+            <Estilos />
             <h1>Freelando</h1>
         </div>
     );
