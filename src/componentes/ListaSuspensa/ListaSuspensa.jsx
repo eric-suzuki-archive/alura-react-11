e73c0d4e3a9fcdaf269917ed00a1b270a8e47178
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-04-07 07:37:22 -0300

    Module 05-Aula 05

--- src/componentes/ListaSuspensa/ListaSuspensa.jsx
+++ src/componentes/ListaSuspensa/ListaSuspensa.jsx
@@ -1,5 +1,5 @@
-import styled from "@emotion/styled"
-import { useState } from "react"
+import styled from '@emotion/styled';
+import { useState } from 'react';
 
 const LabelEstilizada = styled.label`
     display: block;
@@ -9,8 +9,7 @@ const LabelEstilizada = styled.label`
     font-size: 20px;
     line-height: 24px;
     position: relative;
-
-`
+`;
 
 const BotaoEstilizado = styled.button`
     cursor: pointer;
@@ -25,32 +24,39 @@ const BotaoEstilizado = styled.button`
     justify-content: space-between;
     font-weight: 400;
     border-radius: 18px;
-    border-bottom-left-radius: ${props => props.estaAberta ? '0' : '18px'};
-    border-bottom-right-radius: ${props => props.estaAberta ? '0' : '18px'};
-    margin-top: ${props => props.theme.espacamentos.xs};
-    padding: ${props => props.theme.espacamentos.s};
-    background: ${props => props.theme.cores.branco};
-    border: 1px solid ${props => props.theme.cores.neutras.a};
+    border-bottom-left-radius: ${(props) => (props.estaAberta ? '0' : '18px')};
+    border-bottom-right-radius: ${(props) => (props.estaAberta ? '0' : '18px')};
+    margin-top: ${(props) => props.theme.espacamentos.xs};
+    padding: ${(props) => props.theme.espacamentos.s};
+    background: ${(props) => props.theme.cores.branco};
+    border: 1px solid ${(props) => props.theme.cores.neutras.a};
     &:focus {
-        border-color: ${props => props.theme.cores.focus};
+        border-color: ${(props) => props.theme.cores.focus};
     }
-`
+`;
 
 export const ListaSupensa = ({ titulo, opcoes }) => {
-    const [estaAberta, alternarVisibilidade] = useState(false)
+    const [estaAberta, alternarVisibilidade] = useState(false);
 
-    return (<LabelEstilizada>
-        {titulo}
-        <BotaoEstilizado
-             estaAberta={estaAberta}
-             onClick={() => alternarVisibilidade(!estaAberta)}
-        >
-            <div>
-                Selecione
-            </div>
-            <div>
-                <span>{estaAberta ? '▲' : '▼'}</span>
-            </div>
-        </BotaoEstilizado>
-    </LabelEstilizada>)
-}
+    return (
+        <LabelEstilizada>
+            {titulo}
+            <BotaoEstilizado
+                estaAberta={estaAberta}
+                onClick={() => alternarVisibilidade(!estaAberta)}
+            >
+                <div>Selecione</div>
+                <div>
+                    <span>{estaAberta ? '▲' : '▼'}</span>
+                </div>
+            </BotaoEstilizado>
+            {estaAberta && (
+                <ul>
+                    {opcoes.map((opcao) => (
+                        <li key={opcao.value}>{opcao.text}</li>
+                    ))}
+                </ul>
+            )}
+        </LabelEstilizada>
+    );
+};
