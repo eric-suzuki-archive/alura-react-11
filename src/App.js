2854a5ed9a147c3d6b84804271b11335ea3d5806
ERIC YUJI SUZUKI <suzuki.y.eric@gmail.com>
2024-04-07 07:12:08 -0300

    Module 04-Aula 08

--- src/App.js
+++ src/App.js
@@ -9,61 +9,129 @@ function App() {
     return (
         <ProvedorTema>
             <Estilos />
-            <Container style={{ margin: '80px 0' }}>
-                <Row justify='center'>
-                    <Col lg={6} md={8} sm={12}>
-                        <Card>
-                            <Tipografia variante='h1' componente='h1'>
-                                Freelando
+            <Cabecalho>
+                <Container>
+                    <Row align='center'>
+                        <Col>
+                            <FreelandoLogo />
+                        </Col>
+                        <Col style={{ textAlign: 'right' }}>
+                            <Link>Login</Link>
+                        </Col>
+                    </Row>
+                </Container>
+            </Cabecalho>
+            <Row justify='center'>
+                <Col
+                    xxx={6}
+                    xxl={6}
+                    xl={6}
+                    lg={6}
+                    md={8}
+                    sm={12}
+                    style={{ margin: '80px 0' }}
+                >
+                    <Card>
+                        <Tipografia variante='h1' componente='h1'>
+                            Freelando
+                        </Tipografia>
+                        <Tipografia variante='body' componente='body'>
+                            Crie seu perfil gratuitamente para começar a
+                            trabalhar com os melhores freelancers. Em seguida,
+                            você poderá dar mais detalhes sobre suas demandas e
+                            sobre sua forma de trabalho.
+                        </Tipografia>
+                        <Row>
+                            <Col>
+                                <CampoTexto titulo='Nome Completo' />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col lg={4} md={4} sm={4}>
+                                <ListaSupensa
+                                    titulo='Estado'
+                                    opcoes={estadosBrasileiros}
+                                />
+                            </Col>
+                            <Col lg={8} md={8} sm={8}>
+                                <CampoTexto titulo='Cidade' />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <CampoTexto titulo='E-mail' />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col lg={6} md={6} sm={6}>
+                                <CampoTexto titulo='Senha' />
+                            </Col>
+                            <Col lg={6} md={6} sm={6}>
+                                <CampoTexto titulo='Repita a Senha' />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col lg={6} md={6} sm={6}>
+                                <Botao variante='secundaria'>Anterior</Botao>
+                            </Col>
+                            <Col lg={6} md={6} sm={6}>
+                                <div style={{ textAlign: 'right' }}>
+                                    <Botao>Próxima</Botao>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
+            </Row>
+            <Rodape>
+                <Container>
+                    <Row align='center'>
+                        <Col>
+                            <FreelandoLogo height={40} width={176} />
+                            <Tipografia variante='legenda' componente='legenda'>
+                                Desenvolvido por Alura. Projeto fictício sem
+                                fins comerciais.
                             </Tipografia>
-                            <Tipografia variante='body' componente='body'>
-                                Crie seu perfil gratuitamente para começar a
-                                trabalhar com os melhores freelancers. Em
-                                seguida, você poderá dar mais detalhes sobre
-                                suas demandas e sobre sua forma de trabalho.
+                        </Col>
+                        <Col style={{ textAlign: 'right' }}>
+                            <Tipografia variante='legenda' componente='legenda'>
+                                Acesse nossas redes:
                             </Tipografia>
-                            <Row>
-                                <Col>
-                                    <CampoTexto titulo='Nome Completo' />
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col lg={4} md={4} sm={4}>
-                                    <CampoTexto titulo='Estado' />
-                                </Col>
-                                <Col lg={8} md={8} sm={8}>
-                                    <CampoTexto titulo='Cidade' />
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col>
-                                    <CampoTexto titulo='E-mail' />
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col lg={6} md={6} sm={6}>
-                                    <CampoTexto titulo='Senha' />
-                                </Col>
-                                <Col lg={6} md={6} sm={6}>
-                                    <CampoTexto titulo='Repita a Senha' />
-                                </Col>
-                            </Row>
-                            <Row>
-                                <Col lg={6} md={6} sm={6}>
-                                    <Botao variante='secundaria'>
-                                        Anterior
-                                    </Botao>
-                                </Col>
-                                <Col lg={6} md={6} sm={6}>
-                                    <div style={{ textAlign: 'right' }}>
-                                        <Botao>Próxima</Botao>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
+                            <ListaInline>
+                                <ItemListaInline>
+                                    <a
+                                        href='/'
+                                        aria-label='Link para o WhatsApp'
+                                    >
+                                        <IconeWhatsApp />
+                                    </a>
+                                </ItemListaInline>
+                                <ItemListaInline>
+                                    <a href='/' aria-label='Link para a Twitch'>
+                                        <IconeTwitch />
+                                    </a>
+                                </ItemListaInline>
+                                <ItemListaInline>
+                                    <a
+                                        href='/'
+                                        aria-label='Link para a Instagram'
+                                    >
+                                        <IconeInstagram />
+                                    </a>
+                                </ItemListaInline>
+                                <ItemListaInline>
+                                    <a
+                                        href='/'
+                                        aria-label='Link para a Twitter'
+                                    >
+                                        <IconeTwitter />
+                                    </a>
+                                </ItemListaInline>
+                            </ListaInline>
+                        </Col>
+                    </Row>
+                </Container>
+            </Rodape>
         </ProvedorTema>
     );
 }
